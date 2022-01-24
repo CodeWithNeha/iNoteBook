cdc796cabf6f8956f97b739d9489d2dfdc4aff2e
Neha <codewithneha2020@gmail.com>
2022-01-24 22:34:41 +0530

    Login endpoint and sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = 'MongoDBReactExpressNodejs'
 
 
-// Create a User using: POST "/api/auth/createuser". Dosent require auth
+// Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
     body('name','Enter a valid name').isLength({min: 3}),
     body('email','Enter a valid email').isEmail(),
@@ -44,4 +44,35 @@ router.post('/createuser',[
     }
 })
 
+
+// Authenticate user using: POST "/api/auth/login" : No login required
+router.post('/login',[
+  body('email','Enter a valid email').isEmail(),
+  body('password','password cannot be blank').exists(),
+],async(req,res)=>{
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+    try {
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error:"PLEASE TRY TO LOGIN WITH CORRECT CREDENTIALS"})
+      }
+      const passwordCompare = await bcrypt.compare(password,user.password)
+      if(!passwordCompare){
+        return res.status(400).json({error:"PLEASE TRY TO LOGIN WITH CORRECT CREDENTIALS"})
+      }
+      const data={
+        id:user.id
+      }
+      const authToken= jwt.sign(data,JWT_SECRET);
+      res.json({authToken})
+    }    catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server error occured")
+    }
+})
 module.exports = router
