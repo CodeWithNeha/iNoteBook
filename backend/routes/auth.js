9c9b0b2d2fe1c9014c09de6e7bb84f7f68d5819a
Neha <codewithneha2020@gmail.com>
2022-01-24 20:32:05 +0530

    Hashing Password using bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'MongoDBReactExpressNodejs'
+
 
 // Create a User using: POST "/api/auth/createuser". Dosent require auth
 router.post('/createuser',[
@@ -21,13 +25,18 @@ router.post('/createuser',[
     if(user){
       return res.status(400).json({error: "Sorry a user with this email already exists"})
     }
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password,salt)
     user = await User.create({
         name: req.body.name,
         email:req.body.email,
-        password: req.body.password,
-      })
-      
-      res.json({user})
+        password: secPass,
+      });
+      const data={
+        id:user.id
+      }
+      const authToken= jwt.sign(data,JWT_SECRET);
+      res.json({authToken})
     }
     catch(error){
       console.error(error.message);
